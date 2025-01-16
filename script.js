16c7aaddda5fb2e628bb5a592132e27771d70c37
Jesus Reynoso <reynosoprof@gmail.com>
2025-01-16 11:05:01 -0800

    added function to newsletter form to show confirmation

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function clearNewslettertextfield (){
     document.getElementById("emailSignUpEmail").value = ""
 }
+
+function submitNewsletter() {
+    var newsLetterForm = document.getElementById("newsLetterForm");
+    var confirmationMessage = document.getElementById("newslettersignupconfirmation");
+    newsLetterForm.style.visibility = "hidden";
+    confirmationMessage.style.visibility = "visible"
+}
